5b2ced92e69366771465e1ebaf7bd905b95cd69e
glittering-code <glitteringcode9@gmail.com>
2022-03-07 23:24:47 +0530

    string class prototype

---
+++ string/src/string.java
@@ -0,0 +1,15 @@
+public class string {
+  char [] ch;
+  string(char [] chs) {}
+  char charAt(int index) {}
+  int length() {}
+  string substring(int begin) {}
+  string substring(int begin, int int end) {}
+  boolean contains(string s) {}
+  boolean equals(string s) {}
+  string concat(string s) {}
+  string toLowerCase() {}
+  string toUpperCase() {}
+  int indexOf(char ch) {}
+  int indexOf(char ch, int fromIndex) {}
+}
