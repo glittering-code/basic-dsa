7a7a219c75ff5428904efa39b5416c6b44158fa2
glittering-code <99531828+glittering-code@users.noreply.github.com>
2024-08-15 20:38:10 +0530

    Create tripletZero.java

---
+++ Array/tripletZero.java
@@ -0,0 +1,26 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+  // brute force appraoch with n^3 complexity
+    public static boolean checkTripletZero(int [] arr) {
+        for(int i = 0 ; i < arr.length ; ++i) {
+            for(int j = i+1 ; j < arr.length ; ++j) {
+                for(int k = j+1 ; k < arr.length ; ++k) {
+                    if(arr[i] + arr[j] + arr[k] == 0) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,4,5};
+        boolean b = checkTripletZero(arr);
+        System.out.println(b);
+        int [] arr1 = {1,2,-1,-2,-3,0,5};
+        b = checkTripletZero(arr1);
+        System.out.println(b);
+    }
+}
