6556eb03d8fa87696a98a4dcb8f70fa582852289
glittering-code <99531828+glittering-code@users.noreply.github.com>
2024-08-16 18:09:16 +0530

    Create containsDuplicate.java

---
+++ Array/containsDuplicate.java
@@ -0,0 +1,17 @@
+//
+/***************
+  https://leetcode.com/problems/contains-duplicate/
+*******************************/
+class Solution {
+  // brute force solution
+    public boolean containsDuplicate(int[] nums) {
+     for(int i = 0 ; i < nums.length ; ++i) {
+         for(int j = i+1 ; j < nums.length ; ++j) {
+             if(nums[i] == nums[j]) {
+                 return true;
+             }
+         }
+     }
+     return false;   
+    }
+}
